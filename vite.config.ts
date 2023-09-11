0e4c533916dea87b6fc9b68b16d83385cebd009b
Luiyit <luiyit.hernandez@gmail.com>
2023-09-11 09:49:43 -0500

    HMR fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      host: 'localhost',
+    }
+  },
   plugins: [react()],
 })
